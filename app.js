1b77b5695e418d2ab8f71ff77e74a785db2ec0ee
wangyafei <wangyafei@startimes.com.cn>
2017-05-19 13:13:27 +0800

    modify package.json

--- app.js
+++ app.js
@@ -2,28 +2,48 @@
 const Koa = require('koa');
 // 注意require('koa-router')返回的是函数:
 const bodyParser = require('koa-bodyparser');
+//auto config router with koa-router
 //const router = require('koa-router')();
 const controller = require('./controller');
+const templating = require('./templating');
+
+//判断是否是生产环境
+const isProduction = process.env.NODE_ENV === 'production';
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa(); 
 
+//第一个中间件：记录URL以及页面执行时间
 //log request URL:
 app.use(async (ctx, next)=>{
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    console.log(`Process ${ctx.request.method} ${ctx.request.url} ${ctx.request.path}...`);
+    let startTime = new Date().getTime(),
+        execTime;
     await next();
+    execTime = new Date().getTime()-startTime;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
-// router.get('/', async (ctx, next)=>{
-//     ctx.response.body = 'hello';
-// })
 
+//第二个中间件: 处理静态文件
+//set static middleware
+if(!isProduction) {
+    const staticFiles = require('./staticFiles');
+    app.use(staticFiles('/static/', __dirname+'/static'));
+}
 
+//第三个中间件：解析POST请求体body数据
 //add bodyparser middleware:
 app.use(bodyParser());
 //app.use(router.routes());
 
+//第四个中间件：给ctx对象增加render方法
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
 
+//第五个中间件：处理路由
 // add router middleware:
 app.use(controller());
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,12 @@
+//请求根路径的处理函数
+const fn_index = async (ctx, next)=>{
+    ctx.render('index.html', {
+        title: 'Welcome!'
+    });
+};
+
 // 对于任何请求，app将调用该异步函数处理请求：
-const fn_index = async (ctx, next) => {
+const fn_login = async (ctx, next) => {
     ctx.response.body = `<h1>Index</h1>
         <form action="/signin" method="post">
             <p>Name: <input name="name" value="koa"></p>
@@ -12,16 +19,23 @@ const fn_index = async (ctx, next) => {
 //处理登录的post请求
 const fn_signin = async (ctx, next)=>{
     const requestBody = ctx.request.body,
-          name = requestBody.name,
+          email = requestBody.email,
           password = requestBody.password;
-    if(name=='koa'&&password=='12345'){
-        ctx.response.body = `<h1>Hello, ${name}</h1>`;
+    if(email=='1012305328@qq.com'&&password=='123456'){
+        //登录成功
+        ctx.render('signin-ok.html',{
+            title: 'Sign In Ok',
+            name: 'Mr Node'
+        });
     }else {
-        ctx.response.body = `<h1>Login failed, please <a href="/">retry!</a></h1>`
+        ctx.render('signin-failed.html',{
+            title: 'Sign In failed'
+        });
     }
 };
 
 module.exports = {
     'GET /': fn_index,
+    'GET /login': fn_login,
     'POST /signin': fn_signin
 };
